025b112f30b5d164486317cfb35464cd0ecb2e21
AlexSalvatore00 <alexs.marangolo@libero.it>
2021-10-28 09:23:26 +0200

    Branch Master iniziale

---
+++ src/main/java/AlexSalvatoreMarangolo/ProvaIngSw_210284/MyListUtil.java
@@ -0,0 +1,22 @@
+/*
+funzione "sort" che, data una lista di numeri interi,
+organizzi gli elementi sia in ordine crescente sia in ordine decrescente,
+a seconda della scelta dell'utente.
+
+order = 0 //Ascendente
+order = 1 //Dicendente
+
+public List<Integer> sort(List<Integer> list, int order){  }
+            
+Nota:  Si consiglia di scrivere il proprio algoritmo per ordinare i numeri
+*/
+
+package AlexSalvatoreMarangolo.ProvaIngSw_210284;
+
+public class MyListUtil
+{
+	public static void main( String[] args )
+    {
+        //
+    }
+}
---
+++ src/test/java/AlexSalvatoreMarangolo/ProvaIngSw_210284/MyListUtilTest.java
@@ -0,0 +1,14 @@
+package AlexSalvatoreMarangolo.ProvaIngSw_210284;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MyListUtilTest
+{
+	 @Test
+	 public void shouldAnswerWithTrue()
+	 {
+	     assertTrue( true );
+	 }
+}
